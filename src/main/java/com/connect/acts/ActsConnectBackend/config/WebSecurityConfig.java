80912d7dd03bd4eab2fab14b79424a66b181b34a
anurag <anuragch198@gmail.com>
2024-08-14 09:12:53 +0530

    Added JWT functionality -  - Created JwtUtils  - Created JwtEAuthenticationEntryPoint  - Created JwtAuthenticationFilter  - Created WebSecurityConfig  - Create AuthController, AuthService and AuthRepo  - Added functionality of Register  - Added functionality of Login  - Added functionality of generating jwt on register and login
     References Issue #7

---
+++ src/main/java/com/connect/acts/ActsConnectBackend/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.connect.acts.ActsConnectBackend.config;
+
+import com.connect.acts.ActsConnectBackend.security.JwtAuthenticationEntryPoint;
+import com.connect.acts.ActsConnectBackend.security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+  private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+  @Autowired
+  public WebSecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint) {
+    this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+      .csrf(AbstractHttpConfigurer::disable)
+      .authorizeHttpRequests(auth -> auth
+        .requestMatchers("/api/auth/**","/api/home").permitAll()
+        .anyRequest().authenticated()
+      )
+      .exceptionHandling(exception -> exception
+        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+      )
+      .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    return http.build();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+}
